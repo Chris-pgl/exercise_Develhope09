859c85502f0d38e5301794d5210e5af8169b4b3a
Chris <cr1s@hotmail.it>
2022-12-07 16:43:30 +0100

    Add: exercise09

---
+++ src/Starter.java
@@ -0,0 +1,17 @@
+public class Starter {
+    public static void main(String[] args) {
+
+
+        Animal Animal1 = new Animal("Gino");
+        Animal1.animalSound();
+
+        Lion lion1 = new Lion("leo");
+
+        lion1.animalSound();
+
+        Cow cow = new Cow("Gina");
+        cow.animalSound();
+
+
+    }
+}
